53985b0b6b09cf949458d5f90f36c5b16f9c756a
Denis <PlaymarketDK@gmail.com>
2026-03-01 14:44:23 +0300

    Add working PETest

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,20 +1,18 @@
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class PostmanEchoTest {
-}
 
-@Test
-void shouldReturnSentData() {
-    given()
-            .baseUri("https://postman-echo.com")
-            .body("some data")
-            .when()
-            .post("/post")
-            .then()
-            .statusCode(200)
-            .body("data", equalTo("some data"))
-}
+    @Test
+    void shouldReturnSentData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
 }
